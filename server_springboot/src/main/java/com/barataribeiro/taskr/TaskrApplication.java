894df6d3622d1cf0c4367de9f76b2297179f3c0a
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-02-10 10:38:08 -0300

    Add Spring Boot project files

---
+++ server_springboot/src/main/java/com/barataribeiro/taskr/TaskrApplication.java
@@ -0,0 +1,13 @@
+package com.barataribeiro.taskr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskrApplication.class, args);
+	}
+
+}
