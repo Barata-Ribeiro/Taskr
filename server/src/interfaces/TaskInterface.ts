48cf6e54c40fc468b037cb17b833d903ff8bae9d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-02-15 07:15:09 -0300

    fix: adjust TaskInterface with new imports and types

--- server/src/interfaces/TaskInterface.ts
+++ server/src/interfaces/TaskInterface.ts
@@ -1,9 +1,12 @@
-interface RequestingTaskDataBody {
+import { TaskPriority } from "../entities/task/PriorityEnum"
+import { TaskStatus } from "../entities/task/StatusEnum"
+
+export interface RequestingTaskDataBody {
     title: string
     description: string
     projectId: string
     dueDate: string
-    status?: string
-    priority?: string
+    status?: TaskStatus
+    priority?: TaskPriority
     tags?: string[]
 }
