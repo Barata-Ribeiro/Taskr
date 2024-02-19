69bb8380af6806e89c93d07d186bbe74f1271f52
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-02-19 09:08:15 -0300

    feat: add getCommentById method

--- server/src/controllers/CommentController.ts
+++ server/src/controllers/CommentController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express"
 import { validate } from "uuid"
-import { BadRequestError } from "../middlewares/helpers/ApiErrors"
+import { CommentResponseDTO } from "../DTOs/task/CommentResponseDTO"
+import { BadRequestError, NotFoundError } from "../middlewares/helpers/ApiErrors"
+import { commentRepository } from "../repositories/CommentRepository"
 import { CommentService } from "../services/CommentService"
 
 export class CommentController {
@@ -31,14 +33,43 @@ export class CommentController {
     }
 
     public async getCommentById(req: Request, res: Response) {
-        // TODO...
+        const requestingUser = req.user
+        if (!requestingUser?.data?.id) throw new BadRequestError("You must be logged in to update your account.")
+        if (!validate(requestingUser?.data.id)) throw new BadRequestError("Invalid user ID.")
+
+        const { taskId } = req.params
+        if (!taskId) throw new BadRequestError("You must provide a task ID to post a comment.")
+
+        const comment = await commentRepository.findOne({
+            where: { id: taskId },
+            relations: ["creator", "task"]
+        })
+        if (!comment) throw new NotFoundError("Comment not found.")
+
+        const response = CommentResponseDTO.fromEntity(comment)
+
+        return res.status(200).json({
+            status: "success",
+            message: "Comment retrieved successfully.",
+            data: response
+        })
     }
 
     public async updateCommentById(req: Request, res: Response) {
-        // TODO...
+        const requestingUser = req.user
+        if (!requestingUser?.data?.id) throw new BadRequestError("You must be logged in to update your account.")
+        if (!validate(requestingUser?.data.id)) throw new BadRequestError("Invalid user ID.")
+
+        const { taskId } = req.params
+        if (!taskId) throw new BadRequestError("You must provide a task ID to post a comment.")
     }
 
     public async deleteCommentById(req: Request, res: Response) {
-        // TODO...
+        const requestingUser = req.user
+        if (!requestingUser?.data?.id) throw new BadRequestError("You must be logged in to update your account.")
+        if (!validate(requestingUser?.data.id)) throw new BadRequestError("Invalid user ID.")
+
+        const { taskId } = req.params
+        if (!taskId) throw new BadRequestError("You must provide a task ID to post a comment.")
     }
 }
