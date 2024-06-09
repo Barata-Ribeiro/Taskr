e940bb9f916c387385120765947c292e92089d90
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-06-09 10:55:38 -0300

    feat: add ModelMapper

---
+++ server_springboot/src/main/java/com/barataribeiro/taskr/builder/UserMapper.java
@@ -0,0 +1,38 @@
+package com.barataribeiro.taskr.builder;
+
+import com.barataribeiro.taskr.dtos.user.UserDTO;
+import com.barataribeiro.taskr.models.entities.User;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class UserMapper {
+    private final ModelMapper modelMapper;
+
+    public UserDTO toDTO(User user) {
+        return modelMapper.map(user, UserDTO.class);
+    }
+
+    public User toEntity(UserDTO userDTO) {
+        return modelMapper.map(userDTO, User.class);
+    }
+
+    public List<UserDTO> toDTOList(List<User> users) {
+        return users.stream()
+                .map(this::toDTO)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public List<User> toListEntity(List<UserDTO> userDTOS) {
+        return userDTOS.stream()
+                .map(this::toEntity)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+}
