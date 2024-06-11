88104c4d87f14d6b070dc4ec77e1e0807ccffc2c
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-06-11 06:05:26 -0300

    feat: add counters to entities

--- server_springboot/src/main/java/com/barataribeiro/taskr/models/entities/Organization.java
+++ server_springboot/src/main/java/com/barataribeiro/taskr/models/entities/Organization.java
@@ -34,10 +34,34 @@ public class Organization {
 
     private String description;
 
+    @Builder.Default
+    @Column(columnDefinition = "BIGINT default '0'", nullable = false)
+    private Long membersCount = 0L;
+
+    @Builder.Default
+    @Column(columnDefinition = "BIGINT default '0'", nullable = false)
+    private Long projectsCount = 0L;
+
     @Column(updatable = false)
     @CreationTimestamp
     private Instant createdAt;
 
     @UpdateTimestamp
     private Instant updatedAt;
+
+    public void incrementMembersCount() {
+        this.membersCount++;
+    }
+
+    public void decrementMembersCount() {
+        this.membersCount = this.membersCount > 0 ? this.membersCount - 1 : 0;
+    }
+
+    public void incrementProjectsCount() {
+        this.projectsCount++;
+    }
+
+    public void decrementProjectsCount() {
+        this.projectsCount = this.projectsCount > 0 ? this.projectsCount - 1 : 0;
+    }
 }
--- server_springboot/src/main/java/com/barataribeiro/taskr/models/entities/Project.java
+++ server_springboot/src/main/java/com/barataribeiro/taskr/models/entities/Project.java
@@ -37,10 +37,34 @@ public class Project {
     @NotEmpty
     private String description;
 
+    @Builder.Default
+    @Column(columnDefinition = "BIGINT default '0'", nullable = false)
+    private Long membersCount = 0L;
+
+    @Builder.Default
+    @Column(columnDefinition = "BIGINT default '0'", nullable = false)
+    private Long tasksCount = 0L;
+
     @Column(updatable = false)
     @CreationTimestamp
     private Instant createdAt;
 
     @UpdateTimestamp
     private Instant updatedAt;
+
+    public void incrementMembersCount() {
+        this.membersCount++;
+    }
+
+    public void decrementMembersCount() {
+        this.membersCount = this.membersCount > 0 ? this.membersCount - 1 : 0;
+    }
+
+    public void incrementTasksCount() {
+        this.tasksCount++;
+    }
+
+    public void decrementTasksCount() {
+        this.tasksCount = this.tasksCount > 0 ? this.tasksCount - 1 : 0;
+    }
 }
