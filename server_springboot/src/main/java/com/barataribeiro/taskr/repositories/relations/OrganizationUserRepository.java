954535b9313b3fecaed30669ebac948e149fe9bb
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-06-11 09:01:35 -0300

    feat, refactor: add new repository methods, add new param to service

--- server_springboot/src/main/java/com/barataribeiro/taskr/repositories/relations/OrganizationUserRepository.java
+++ server_springboot/src/main/java/com/barataribeiro/taskr/repositories/relations/OrganizationUserRepository.java
@@ -2,6 +2,18 @@ package com.barataribeiro.taskr.repositories.relations;
 
 import com.barataribeiro.taskr.models.relations.OrganizationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface OrganizationUserRepository extends JpaRepository<OrganizationUser, Long> {
+    @Query("select (count(o) > 0) from OrganizationUser o where o.user.id = :userId and o.isOwner = :isOwner")
+    boolean existsOrganizationWhereUserByIdIsOwner(String userId, boolean isOwner);
+
+    boolean existsByUser_Id(String id);
+
+    @Query("select o from OrganizationUser o where o.organization.id = :id " +
+            "and o.user.username = :username and o.isOwner = :isOwner")
+    Optional<OrganizationUser> findOrganizationByUser_UsernameAndIsOwner(Integer id, String username, boolean isOwner);
+
 }
---
+++ server_springboot/src/main/java/com/barataribeiro/taskr/services/OrganizationService.java
@@ -0,0 +1,18 @@
+package com.barataribeiro.taskr.services;
+
+import com.barataribeiro.taskr.dtos.organization.OrganizationDTO;
+import com.barataribeiro.taskr.dtos.organization.OrganizationRequestDTO;
+import com.barataribeiro.taskr.dtos.organization.UpdateOrganizationRequestDTO;
+
+import java.security.Principal;
+import java.util.Map;
+
+public interface OrganizationService {
+    OrganizationDTO createOrganization(OrganizationRequestDTO body, Principal principal);
+
+    OrganizationDTO getOrganizationInfo(String id);
+
+    Map<String, Object> updateOrganizationInfo(String id, UpdateOrganizationRequestDTO body, Principal principal);
+
+    void deleteOrganization(String id, Principal principal);
+}
