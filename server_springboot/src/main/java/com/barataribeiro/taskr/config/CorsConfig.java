30bfbb5d3dc83a9bb2e8620680c81df4b8aa6693
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-06-18 18:26:01 -0300

    fix: add missing not null annotation

--- server_springboot/src/main/java/com/barataribeiro/taskr/config/CorsConfig.java
+++ server_springboot/src/main/java/com/barataribeiro/taskr/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.barataribeiro.taskr.config;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,13 +10,12 @@ import java.util.Arrays;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    private static final long MAX_AGE_SECS = 3600;
     @Value("${api.security.cors.origins}")
     private String ALLOWED_ORIGINS;
 
-    private static final long MAX_AGE_SECS = 3600;
-
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NotNull CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(Arrays.stream(ALLOWED_ORIGINS.split(",")).toArray(String[]::new))
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
