d8677705a7359a3425dc9da8195a1ae9db3a6d76
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-06-24 06:39:46 -0300

    refactor: override equals, hashCode and toString in record DTOs

--- server_springboot/src/main/java/com/barataribeiro/taskr/dtos/organization/ManagementRequestDTO.java
+++ server_springboot/src/main/java/com/barataribeiro/taskr/dtos/organization/ManagementRequestDTO.java
@@ -1,7 +1,32 @@
 package com.barataribeiro.taskr.dtos.organization;
 
-import java.io.Serializable;
+import org.jetbrains.annotations.NotNull;
 
-public record ManagementRequestDTO(String[] usersToAdd,
-                                   String[] usersToRemove) implements Serializable {
+import java.io.Serializable;
+import java.util.Arrays;
+
+public record ManagementRequestDTO(String[] usersToAdd, String[] usersToRemove) implements Serializable {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ManagementRequestDTO that = (ManagementRequestDTO) o;
+        return Arrays.equals(usersToAdd, that.usersToAdd) && Arrays.equals(usersToRemove, that.usersToRemove);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Arrays.hashCode(usersToAdd);
+        result = 31 * result + Arrays.hashCode(usersToRemove);
+        return result;
+    }
+
+    @Override
+    public @NotNull String toString() {
+        return "ManagementRequestDTO{" +
+                "usersToAdd=" + Arrays.toString(usersToAdd) +
+                ", usersToRemove=" + Arrays.toString(usersToRemove) +
+                '}';
+    }
 }
