c7175e79ce183ca7b1d3fc9776b1c85e88b1dc1a
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-07-02 15:21:15 -0300

    feat: add hero section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,17 @@
 import Footer from "@/components/home/footer"
 import Header from "@/components/home/header"
+import HeroSection from "@/components/home/hero-section"
 import { Fragment } from "react"
 
 export default function Home() {
     return (
         <Fragment>
             <Header />
-            <main className="container h-screen">
-                <h1>Welcome to Test!</h1>
+            <main className="snap-y scroll-smooth">
+                <HeroSection />
+                <section id="about" className="container snap-start"></section>
+                <section id="features" className="container snap-start"></section>
+                <section id="contact" className="container snap-start"></section>
             </main>
             <Footer />
         </Fragment>
