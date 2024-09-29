d16542a9df6bbf94b38f762f296489e52773522a
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-09-29 08:17:22 -0300

    feat: add new config support, implement secret key gen

--- server/src/main/java/com/barataribeiro/taskr/ServerSpringbootApplication.java
+++ server/src/main/java/com/barataribeiro/taskr/ServerSpringbootApplication.java
@@ -2,12 +2,16 @@ package com.barataribeiro.taskr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(
+        pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
 public class ServerSpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerSpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSpringbootApplication.class, args);
+    }
 
 }
