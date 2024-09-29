964bb3eff8ec2f8724b5e87dc604d20161f7e5b6
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-09-29 09:38:30 -0300

    refactor: use lombok annotations for equals and hashcode

--- server/src/main/java/com/barataribeiro/taskr/models/embeddables/OrganizationUserId.java
+++ server/src/main/java/com/barataribeiro/taskr/models/embeddables/OrganizationUserId.java
@@ -1,12 +1,7 @@
 package com.barataribeiro.taskr.models.embeddables;
 
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import lombok.*;
 
 import java.io.Serializable;
 
@@ -14,25 +9,9 @@ import java.io.Serializable;
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode
 @Embeddable
 public class OrganizationUserId implements Serializable {
     private Long organizationId;
     private String userId;
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37).append(getOrganizationId()).append(getUserId()).toHashCode();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (!(o instanceof OrganizationUserId that)) return false;
-
-        return new EqualsBuilder()
-                .append(getOrganizationId(), that.getOrganizationId())
-                .append(getUserId(), that.getUserId())
-                .isEquals();
-    }
 }
--- server/src/main/java/com/barataribeiro/taskr/models/embeddables/ProjectTaskId.java
+++ server/src/main/java/com/barataribeiro/taskr/models/embeddables/ProjectTaskId.java
@@ -1,12 +1,7 @@
 package com.barataribeiro.taskr.models.embeddables;
 
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import lombok.*;
 
 import java.io.Serializable;
 
@@ -14,23 +9,9 @@ import java.io.Serializable;
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode
 @Embeddable
 public class ProjectTaskId implements Serializable {
     private Long projectId;
     private Long taskId;
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37).append(getProjectId()).append(getTaskId()).toHashCode();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (!(o instanceof ProjectTaskId that)) return false;
-
-        return new EqualsBuilder().append(getProjectId(), that.getProjectId()).append(
-                getTaskId(), that.getTaskId()).isEquals();
-    }
 }
