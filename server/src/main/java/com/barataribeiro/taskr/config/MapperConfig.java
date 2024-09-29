ee20aa305b2b83d262d8daaf8b0e401531dcdc27
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-09-29 08:12:52 -0300

    refactor: adjust logging and config

--- server/src/main/java/com/barataribeiro/taskr/config/MapperConfig.java
+++ server/src/main/java/com/barataribeiro/taskr/config/MapperConfig.java
@@ -1,6 +1,7 @@
 package com.barataribeiro.taskr.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,13 @@ import org.springframework.context.annotation.Configuration;
 public class MapperConfig {
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration().setFieldMatchingEnabled(true)
+                   .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                   .setMatchingStrategy(MatchingStrategies.STRICT)
+                   .setSkipNullEnabled(true);
+
+        return modelMapper;
     }
 }
