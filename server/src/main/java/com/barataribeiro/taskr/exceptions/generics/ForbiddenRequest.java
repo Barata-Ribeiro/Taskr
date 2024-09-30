585a8626fdab210730bea5a9ef079a8739444ee0
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-09-30 06:45:20 -0300

    remove: delete unused exceptions

--- server/src/main/java/com/barataribeiro/taskr/exceptions/generics/ForbiddenRequest.java
+++
@@ -1,7 +0,0 @@
-package com.barataribeiro.taskr.exceptions.generics;
-
-public class ForbiddenRequest extends RuntimeException {
-    public ForbiddenRequest() {
-        super("You do not have permission to access this resource.");
-    }
-}
