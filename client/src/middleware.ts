c1846befbdbfbd98899802c9a5611e6465f73ea7
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-10-04 09:29:51 -0300

    refactor: adjust middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -3,15 +3,17 @@ import verifyToken from "@/utils/verify-token"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function middleware(req: NextRequest) {
-    const auth_token = req.cookies.get("auth_token")?.value
-    const authenticated = auth_token ? await verifyToken(auth_token) : false
+    const accessToken = req.cookies.get("accessToken")?.value
+    const { pathname } = req.nextUrl
+    const clonedUrl = req.nextUrl.clone()
+    const isAuthenticated = accessToken ? await verifyToken(accessToken) : false
 
-    if (!authenticated && req.nextUrl.pathname.startsWith("/dashboard")) {
-        return NextResponse.redirect(new URL("/sign/in", req.url))
+    if (!isAuthenticated && (clonedUrl.pathname === "/" || pathname.startsWith("/dashboard"))) {
+        return NextResponse.redirect(new URL("/auth/login", req.url))
     }
 
-    if (authenticated && req.nextUrl.pathname.startsWith("/sign")) {
-        const jwtPayload = await decodeToken(auth_token)
+    if (isAuthenticated && (clonedUrl.pathname === "/" || pathname.startsWith("/auth"))) {
+        const jwtPayload = await decodeToken(accessToken)
         return NextResponse.redirect(new URL("/dashboard/" + jwtPayload, req.url))
     }
 
@@ -19,5 +21,10 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/dashboard/:path*", "/sign/:path*", "/((?!api|_next/static|_next/image|favicon.ico).*)"],
+    matcher: [
+        "/((?!_next|[^?]*\\\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+        "/(api|trpc)(.*)",
+        "/dashboard/:path*",
+        "/auth/:path*",
+    ],
 }
