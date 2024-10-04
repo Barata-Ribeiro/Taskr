db28bec94a7d61dad88adb36c629eee82097f8d6
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-10-04 08:30:00 -0300

    refactor: add correct user DTO on return

--- server/src/main/java/com/barataribeiro/taskr/dtos/auth/LoginResponseDTO.java
+++ server/src/main/java/com/barataribeiro/taskr/dtos/auth/LoginResponseDTO.java
@@ -1,13 +1,14 @@
 package com.barataribeiro.taskr.dtos.auth;
 
-import com.barataribeiro.taskr.dtos.user.UserDTO;
+import com.barataribeiro.taskr.dtos.user.ContextDTO;
 import jakarta.annotation.Nullable;
 
 import java.io.Serializable;
 import java.time.Instant;
 
-public record LoginResponseDTO(UserDTO user,
+public record LoginResponseDTO(ContextDTO user,
                                String accessToken,
+                               Instant accessTokenExpiresAt,
                                @Nullable String refreshToken,
                                @Nullable Instant refreshTokenExpiresAt) implements Serializable {
 }
