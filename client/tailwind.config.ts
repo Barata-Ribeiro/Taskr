e8e31500f090d50c7d0b4306879c99d6a8a0fbaa
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-10-11 05:49:31 -0300

    raw: adjust tailwind config plugins

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -107,6 +107,7 @@ const config: Config = {
             },
         },
     },
-    plugins: [require("@tailwindcss/forms")({ strategy: "class" })],
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 }
 export default config
