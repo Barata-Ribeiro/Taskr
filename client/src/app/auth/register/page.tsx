b99b373a640d3f25d8af41753b4d955c024a3fe0
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-10-12 08:21:42 -0300

    feat: implement registration route and form

--- client/src/app/auth/register/page.tsx
+++ client/src/app/auth/register/page.tsx
@@ -1,11 +1,12 @@
-import { Button, Field, Input, Label } from "@headlessui/react"
+import RegisterForm from "@/components/forms/register-form"
 import { Metadata } from "next"
 import Image from "next/image"
 import Link from "next/link"
+import { FaCircleInfo } from "react-icons/fa6"
 import taskrLogo from "../../../../public/images/logo.svg"
 
 export const metadata: Metadata = {
-    title: "Register",
+    title: "Registration",
     description: "Register for a Taskr account",
 }
 
@@ -20,68 +21,21 @@ export default function RegisterPage() {
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-                <div className="bg-white px-6 py-12 shadow-derek sm:rounded-lg sm:px-12">
-                    <form action="#" method="POST" className="space-y-6">
-                        <Field>
-                            <Label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                Email address
-                            </Label>
-                            <div className="mt-2">
-                                <Input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    required
-                                    autoComplete="email"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-ebony-600 sm:text-sm sm:leading-6"
-                                />
+                <div className="bg-white px-6 pb-12 pt-6 shadow-derek sm:rounded-lg sm:px-12">
+                    <RegisterForm />
+                    <div className="mt-6 border-l-4 border-blue-400 bg-blue-50 p-4">
+                        <div className="flex">
+                            <div className="flex-shrink-0">
+                                <FaCircleInfo aria-hidden="true" className="h-5 w-5 text-blue-400" />
                             </div>
-                        </Field>
-
-                        <Field>
-                            <Label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                Password
-                            </Label>
-                            <div className="mt-2">
-                                <Input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    required
-                                    autoComplete="current-password"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-ebony-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </Field>
-
-                        <div className="flex items-center justify-between">
-                            <Field className="flex items-center">
-                                <Input
-                                    id="rememberMe"
-                                    name="rememberMe"
-                                    type="checkbox"
-                                    className="h-4 w-4 rounded border-gray-300 text-english-holly-600 focus:ring-english-holly-600"
-                                />
-                                <Label htmlFor="rememberMe" className="ml-3 block text-sm leading-6 text-gray-900">
-                                    Remember me
-                                </Label>
-                            </Field>
-
-                            <div className="text-sm leading-6">
-                                <Link
-                                    href="/auth/forgot-password"
-                                    className="font-semibold text-ebony-600 decoration-2 underline-offset-4 hover:text-ebony-700 hover:underline active:text-ebony-800">
-                                    Forgot password?
-                                </Link>
+                            <div className="ml-3">
+                                <p className="text-sm text-blue-700">
+                                    By signing up to create an account, you are accepting our terms of service and
+                                    privacy policy.
+                                </p>
                             </div>
                         </div>
-
-                        <Button
-                            type="submit"
-                            className="flex w-full justify-center rounded-md bg-ebony-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-ebony-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-ebony-600">
-                            Sign in
-                        </Button>
-                    </form>
+                    </div>
                 </div>
 
                 <p className="mt-10 text-center text-sm text-gray-100">
--- client/src/components/forms/register-form.tsx
+++ client/src/components/forms/register-form.tsx
@@ -1,8 +1,11 @@
 "use client"
 
 import postAuthRegister from "@/actions/auth/post-auth-register"
+import ApiRequestError from "@/components/feedback/api-request-error"
+import InputValidationError from "@/components/feedback/input-validation-error"
 import Spinner from "@/components/helpers/spinner"
 import { useForm } from "@/hooks/use-form"
+import { ProblemDetails } from "@/interfaces/actions"
 import { Button, Field, Input, Label } from "@headlessui/react"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
@@ -22,7 +25,7 @@ export default function RegisterForm() {
     }, [formState.ok, router])
 
     return (
-        <form action={formAction} onSubmit={onSubmit} className="space-y-6">
+        <form action={formAction} onSubmit={onSubmit} className="space-y-6 overflow-y-auto">
             <Field>
                 <Label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
                     Username
@@ -76,13 +79,30 @@ export default function RegisterForm() {
                     aria-required
                 />
             </Field>
+            <Field>
+                <Label htmlFor="confirmPassword" className="block text-sm font-medium leading-6 text-gray-900">
+                    Confirm Password
+                </Label>
+                <Input
+                    type="password"
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-ebony-600 sm:text-sm sm:leading-6"
+                    required
+                    aria-required
+                />
+            </Field>
 
-            {/*TODO: PUT ERRORS HERE*/}
+            {formState.error && !Array.isArray(formState.error) && (
+                <ApiRequestError error={formState.error as ProblemDetails} />
+            )}
+
+            {formState.error && Array.isArray(formState.error) && <InputValidationError errors={formState.error} />}
 
             <Button
                 type="submit"
                 disabled={isPending}
-                className="inline-flex w-full justify-center rounded-md bg-ebony-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-ebony-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-ebony-600">
+                className="inline-flex w-full items-center justify-center rounded-md bg-ebony-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-ebony-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-ebony-600 active:bg-ebony-800 disabled:opacity-50">
                 {isPending ? (
                     <>
                         <Spinner /> Loading...
