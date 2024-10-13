01b0560904deec01d4af7070c4926e65677e420d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-10-13 08:00:55 -0300

    refactor: implement better password field and routing on success

--- client/src/components/forms/register-form.tsx
+++ client/src/components/forms/register-form.tsx
@@ -6,6 +6,7 @@ import InputValidationError from "@/components/feedback/input-validation-error"
 import Spinner from "@/components/helpers/spinner"
 import { useForm } from "@/hooks/use-form"
 import { ProblemDetails } from "@/interfaces/actions"
+import { User } from "@/interfaces/user"
 import { Button, Field, Input, Label } from "@headlessui/react"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
@@ -20,9 +21,12 @@ export default function RegisterForm() {
 
     useEffect(() => {
         if (formState.ok) {
-            router.replace("/auth/login")
+            const response = formState.response
+            router.replace(
+                `/auth/login?success=${response?.code === 201}&message=${response?.message}&username=${(response?.data as User).username}`,
+            )
         }
-    }, [formState.ok, router])
+    }, [formState, router])
 
     return (
         <form action={formAction} onSubmit={onSubmit} className="space-y-6 overflow-y-auto">
@@ -65,33 +69,35 @@ export default function RegisterForm() {
                     aria-required
                 />
             </Field>
-            <Field>
-                <Label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
-                </Label>
-                <Input
-                    type="password"
-                    id="password"
-                    name="password"
-                    autoComplete="new-password"
-                    className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-ebony-600 sm:text-sm sm:leading-6"
-                    required
-                    aria-required
-                />
-            </Field>
-            <Field>
-                <Label htmlFor="confirmPassword" className="block text-sm font-medium leading-6 text-gray-900">
-                    Confirm Password
-                </Label>
-                <Input
-                    type="password"
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-ebony-600 sm:text-sm sm:leading-6"
-                    required
-                    aria-required
-                />
-            </Field>
+            <div className="isolate -space-y-px rounded-md shadow-sm">
+                <Field className="relative rounded-md rounded-b-none px-3 pb-1.5 pt-2.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-ebony-600">
+                    <Label htmlFor="password" className="block text-xs font-medium text-gray-900">
+                        Password
+                    </Label>
+                    <Input
+                        id="password"
+                        name="password"
+                        type="password"
+                        className="block w-full rounded border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                        autoComplete="new-password"
+                        required
+                        aria-required
+                    />
+                </Field>
+                <Field className="relative rounded-md rounded-t-none px-3 pb-1.5 pt-2.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-ebony-600">
+                    <Label htmlFor="confirmPassword" className="block text-xs font-medium text-gray-900">
+                        Confirm Password
+                    </Label>
+                    <Input
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        type="password"
+                        className="block w-full rounded border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                        required
+                        aria-required
+                    />
+                </Field>
+            </div>
 
             {formState.error && !Array.isArray(formState.error) && (
                 <ApiRequestError error={formState.error as ProblemDetails} />
