3292d6b0f2c27808e3e4437136e15a3c0d823f28
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-10-13 08:57:49 -0300

    fix: add correct render style

--- client/src/app/auth/layout.tsx
+++ client/src/app/auth/layout.tsx
@@ -1,11 +1,9 @@
 import tw from "@/utils/tw"
-import { auth } from "auth"
 import { ReactNode } from "react"
 import authImage from "../../../public/images/auth-image.jpg"
 
 export default async function AuthLayout({ children }: Readonly<{ children: ReactNode }>) {
-    const session = await auth()
-    const mainStyles = tw`relative h-screen w-screen bg-ebony-900 bg-opacity-60 bg-cover bg-center bg-no-repeat bg-blend-multiply`
+    const mainStyles = tw`relative min-h-screen w-auto bg-ebony-900 bg-opacity-60 bg-cover bg-fixed bg-center bg-no-repeat bg-blend-multiply`
 
     return (
         <main className={mainStyles} style={{ backgroundImage: `url(${authImage.src}` }}>
