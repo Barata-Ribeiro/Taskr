a8f0d85bf98da92153234c7ff66f7607bce1f84e
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-10-14 06:51:40 -0300

    feat: implement dashboard routes layout

---
+++ client/src/app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "@/components/sidebar"
+import { auth } from "auth"
+import { redirect } from "next/navigation"
+import { ReactNode } from "react"
+
+export default async function DashboardLayout({ children }: Readonly<{ children: ReactNode }>) {
+    const session = await auth()
+    if (!session) return redirect("/auth/login")
+
+    return <Sidebar session={session}>{children}</Sidebar>
+}
