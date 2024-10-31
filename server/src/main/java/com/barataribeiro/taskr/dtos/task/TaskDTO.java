85c26b78679e27bb3f7379c0c1e9b1763c6a43f5
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-10-31 06:11:34 -0300

    fix: add missing property, adjust types

--- server/src/main/java/com/barataribeiro/taskr/dtos/task/TaskDTO.java
+++ server/src/main/java/com/barataribeiro/taskr/dtos/task/TaskDTO.java
@@ -3,27 +3,30 @@ package com.barataribeiro.taskr.dtos.task;
 import com.barataribeiro.taskr.models.enums.TaskPriority;
 import com.barataribeiro.taskr.models.enums.TaskStatus;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDate;
 
 /**
  * DTO for {@link com.barataribeiro.taskr.models.entities.Task}
  */
-@NoArgsConstructor
+@Data
+@Builder
 @AllArgsConstructor
-@Getter
-@Setter
-@ToString
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TaskDTO implements Serializable {
-    private Integer id;
+    private Long id;
     private String title;
     private String description;
     private TaskStatus status;
     private TaskPriority priority;
-    private Date dueDate;
+    private LocalDate startDate;
+    private LocalDate dueDate;
     private String createdAt;
     private String updatedAt;
 }
