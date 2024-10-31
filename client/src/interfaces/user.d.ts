4f564eb7cc439a1345a7d41adebf77da4d0f68a7
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-10-31 06:29:07 -0300

    feat, refactor: add get public profile server action, adjust profile type

--- client/src/interfaces/user.d.ts
+++ client/src/interfaces/user.d.ts
@@ -19,7 +19,7 @@ interface User {
     updatedAt: string
 }
 
-interface Profile {
+interface PublicUser {
     id: string
     username: string
     displayName: string
@@ -29,6 +29,11 @@ interface Profile {
     role: UserRoles
     createdAt: string
     updatedAt: string
+}
+
+interface Profile {
+    profile: PublicUser
+    organizationsWhichUserIsMember: SimpleOrganization[]
 }
 
 interface UserContext {
@@ -48,8 +53,17 @@ interface UserDashboard {
 type OrganizationMemberRole = "Admin" | "Owner" | "Member"
 
 interface OrganizationMember {
-    user: Profile
+    user: PublicUser
     roles: OrganizationMemberRole[]
 }
 
-export type { UserRoles, User, Profile, UserContext, UserDashboard, OrganizationMemberRole, OrganizationMember }
+export type {
+    UserRoles,
+    User,
+    PublicUser,
+    Profile,
+    UserContext,
+    UserDashboard,
+    OrganizationMemberRole,
+    OrganizationMember,
+}
