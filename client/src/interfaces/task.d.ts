6097793945deeb95025f21c3ac911a6c0c50e319
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-11-07 07:06:29 -0300

    feat: add task retrieval functionality and update task interfaces
    - Add `CompleteTask`, `SortedTasks`, and `ProjectSortedTasks` interfaces to `task.d.ts`
    - Add `TASKS_GET_ALL_BY_PROJECT_ID` URL to `api-urls.ts`
    - Implement `getAllTasksByProjectId` action to fetch all tasks by project ID

--- client/src/interfaces/task.d.ts
+++ client/src/interfaces/task.d.ts
@@ -1,3 +1,6 @@
+import { Project } from "@/interfaces/project"
+import { User } from "@/interfaces/user"
+
 type TaskStatus = "OPEN" | "IN_PROGRESS" | "IN_REVIEW" | "DONE"
 type TaskPriority = "LOW" | "MEDIUM" | "HIGH"
 
@@ -13,4 +16,21 @@ interface Task {
     updatedAt: string
 }
 
-export type { Task, TaskStatus, TaskPriority }
+interface CompleteTask {
+    task: Task
+    userAssigned: User
+    userCreator: User
+}
+
+interface SortedTasks {
+    lowPriority: CompleteTask[]
+    mediumPriority: CompleteTask[]
+    highPriority: CompleteTask[]
+}
+
+interface ProjectSortedTasks {
+    project: Project
+    tasks: SortedTasks
+}
+
+export type { TaskStatus, TaskPriority, Task, CompleteTask, SortedTasks, ProjectSortedTasks }
--- client/src/utils/api-urls.ts
+++ client/src/utils/api-urls.ts
@@ -61,3 +61,7 @@ export const PROJECTS_POST_NEW = (orgId: string) => `${BACKEND_URL}/api/v1/proje
 export const PROJECTS_GET_BY_ORG_ID_AND_PROJECT_ID = (orgId: number, projectId: number) =>
     `${BACKEND_URL}/api/v1/projects/${orgId}/project/${projectId}`
 export const PROJECTS_GET_OWN_PROJECTS_BY_ORG_ID = (orgId: number) => `${BACKEND_URL}/api/v1/projects/${orgId}/me`
+
+// Tasks
+export const TASKS_GET_ALL_BY_PROJECT_ID = (orgId: number, projectId: number) =>
+    `${BACKEND_URL}/api/v1/projects/${orgId}/project/${projectId}/tasks`
