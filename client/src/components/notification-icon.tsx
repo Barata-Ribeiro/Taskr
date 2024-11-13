bae3aaf87cd7a34b5bbf972133ea34b1ca909824
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-11-13 11:24:16 -0300

    feat: enhance NotificationIcon with Link and Tailwind styles
    - Added `Link` component to navigate to notifications page.
    - Integrated `twMerge` for conditional Tailwind CSS classes.
    - Updated notification icon styles for better accessibility and appearance.

--- client/src/components/notification-icon.tsx
+++ client/src/components/notification-icon.tsx
@@ -2,8 +2,10 @@
 
 import { User } from "@/interfaces/user"
 import { useWebsocket } from "@/providers/websocket-provider"
+import Link from "next/link"
 import { useEffect, useState } from "react"
 import { FaBell } from "react-icons/fa6"
+import { twMerge } from "tailwind-merge"
 
 export default function NotificationIcon({ context }: Readonly<{ context: User }>) {
     const { notifications } = useWebsocket()
@@ -16,16 +18,25 @@ export default function NotificationIcon({ context }: Readonly<{ context: User }
         }
     }, [notifications])
 
+    const unreadCounterBaseStyles =
+        "absolute bottom-auto left-0 right-auto top-0 z-10 inline-block -translate-y-1/3 translate-x-1.5 rotate-0 skew-x-0 skew-y-0 scale-x-75 scale-y-75 select-none whitespace-nowrap rounded-full px-2.5 py-1 text-center align-baseline text-xs font-bold leading-none text-white"
+
     return (
-        <div>
-            <button className="relative">
-                <FaBell aria-hidden="true" className="h-6 w-6 text-gray-700" />
-                {unreadCount > 0 && (
-                    <span className="absolute right-0 top-0 inline-flex items-center justify-center rounded-full bg-red-600 px-2 py-1 text-xs font-bold leading-none text-red-100">
-                        {unreadCount}
-                    </span>
-                )}
-            </button>
-        </div>
+        <Link
+            href="/dashboard/profile/notifications"
+            className="relative rounded-full bg-ebony-50 p-1 text-gray-400 hover:text-gray-50 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 disabled:cursor-default disabled:opacity-50"
+            aria-label="View notifications"
+            title="View notifications">
+            <FaBell aria-hidden="true" className="h-5 w-5 text-ebony-500" />
+            {unreadCount > 0 && (
+                <span
+                    className={twMerge(
+                        unreadCounterBaseStyles,
+                        unreadCount === 0 ? "bg-neutral-600" : "bg-english-holly-600",
+                    )}>
+                    {unreadCount > 99 ? "99+" : unreadCount}
+                </span>
+            )}
+        </Link>
     )
 }
