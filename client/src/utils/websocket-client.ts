75ad660c629f7849083140ec764bd091de3f94cd
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-11-13 08:23:33 -0300

    feat: add WebSocket client utility
    - Implemented `WebSocketClient` class to manage WebSocket connections using `@stomp/stompjs` and `sockjs-client`.
    - Added methods for connecting, disconnecting, subscribing, unsubscribing, and sending messages.
    - Configured WebSocket client with heartbeat and reconnect delay.

---
+++ client/src/utils/websocket-client.ts
@@ -0,0 +1,71 @@
+import { Client, IMessage, StompSubscription } from "@stomp/stompjs"
+import SockJS from "sockjs-client"
+
+const BACKEND_URL = process.env.BACKEND_ORIGIN ?? "http://localhost:8080"
+
+class WebSocketClient {
+    private readonly client: Client
+    private readonly subscriptions: { [key: string]: StompSubscription } = {}
+    private readonly onConnectCallbacks: Array<() => void> = []
+
+    constructor() {
+        this.client = new Client({
+            webSocketFactory: () => new SockJS(`${BACKEND_URL}/ws`),
+            reconnectDelay: 5000,
+            heartbeatIncoming: 10000,
+            heartbeatOutgoing: 10000,
+            logRawCommunication: true,
+            debug: str => console.log(str),
+        })
+
+        this.client.onConnect = () => {
+            console.log("Connected to WebSocket")
+            this.onConnectCallbacks.forEach(callback => callback())
+        }
+
+        this.client.onStompError = frame => {
+            console.error("Broker reported error: " + frame.headers["message"])
+            console.error("Additional details: " + frame.body)
+        }
+    }
+
+    registerOnConnectCallback(callback: () => void) {
+        this.onConnectCallbacks.push(callback)
+    }
+
+    configureHeaders(headers: { [key: string]: string }) {
+        this.client.configure({
+            connectHeaders: headers,
+        })
+    }
+
+    connect(headers: { [key: string]: string } = {}) {
+        this.configureHeaders(headers)
+
+        if (!this.client.active) this.client.activate()
+    }
+
+    disconnect() {
+        if (this.client.active)
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            this.client.deactivate().then(_r => console.log("Disconnected from WebSocket"))
+    }
+
+    subscribe(destination: string, callback: (message: IMessage) => void): StompSubscription {
+        return this.client.subscribe(destination, callback)
+    }
+
+    unSubscribe(destination: string) {
+        if (this.subscriptions[destination]) {
+            this.subscriptions[destination].unsubscribe()
+            delete this.subscriptions[destination]
+        }
+    }
+
+    send(destination: string, body: string, headers: { [key: string]: string } = {}) {
+        this.client.publish({ destination, body: JSON.stringify(body), headers })
+    }
+}
+
+const websocketClient = new WebSocketClient()
+export default websocketClient
