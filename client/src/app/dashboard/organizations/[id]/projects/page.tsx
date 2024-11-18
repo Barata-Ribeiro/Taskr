84486e80af362c1382b237c7caca550f98c51d01
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-11-18 06:19:24 -0300

    feat: add project list table to organization projects page
    - Changed `div` to `section` for better semantic HTML.
    - Added a table to display project details including ID, title, members count, tasks count, deadline, and status.
    - Included `NavigationPagination` for pagination support.

--- client/src/app/dashboard/organizations/[id]/projects/page.tsx
+++ client/src/app/dashboard/organizations/[id]/projects/page.tsx
@@ -1,8 +1,12 @@
 import getOrganizationProjectsById from "@/actions/organizations/get-organization-projects-by-id"
 import StateError from "@/components/feedback/state-error"
+import NavigationPagination from "@/components/filters/navigation-pagination"
 import TableCompleteFilter from "@/components/filters/table-complete-filter"
+import ProjectStatusBadge from "@/components/helpers/project-status-badge"
 import { ProblemDetails } from "@/interfaces/actions"
 import { OrganizationProjectsList } from "@/interfaces/organization"
+import parseDate from "@/utils/parse-date"
+import Link from "next/link"
 import { notFound } from "next/navigation"
 
 interface OrganizationProjectsPageProps {
@@ -56,7 +60,7 @@ export default async function OrganizationProjectsPage({
     const pageInfo = pagination.page
 
     return (
-        <div
+        <section
             id="organization-projects-list"
             aria-labelledby="organization-projects-list-title"
             className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -71,6 +75,95 @@ export default async function OrganizationProjectsPage({
             </header>
 
             <TableCompleteFilter filterType="projects" />
-        </div>
+
+            <div className="mt-8 flow-root">
+                <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                        <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+                            <table className="min-w-full divide-y divide-gray-300">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        <th
+                                            scope="col"
+                                            className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                            Id
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                            Title
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                            Members Count
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                            Tasks Count
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                            Deadline
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                            Status
+                                        </th>
+                                        <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                            <span className="sr-only">View Project</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className="divide-y divide-gray-200 bg-white">
+                                    {content.map((pivot, idx) => (
+                                        <tr key={idx} className="border-b border-gray-300">
+                                            <td className="py-4 pl-4 pr-3 text-sm text-gray-900 sm:pl-6 lg:pl-8">
+                                                {pivot.project.id}
+                                            </td>
+                                            <td className="px-3 py-4 text-sm text-gray-900">{pivot.project.name}</td>
+                                            <td className="px-3 py-4 text-sm text-gray-900">
+                                                {pivot.project.membersCount}
+                                            </td>
+                                            <td className="px-3 py-4 text-sm text-gray-900">
+                                                {pivot.project.tasksCount}
+                                            </td>
+                                            <td className="px-3 py-4 text-sm text-gray-900">
+                                                {parseDate(pivot.project.deadline)}
+                                            </td>
+                                            <td className="px-3 py-4">
+                                                <ProjectStatusBadge type="text-only" status={pivot.status} />
+                                            </td>
+                                            <td align="right" className="py-4 pl-3 pr-4 sm:pr-6">
+                                                <Link
+                                                    href={`/dashboard/organizations/${organization.id}/projects/${pivot.project.id}`}
+                                                    className="inline-block w-max rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 active:bg-gray-100">
+                                                    View project
+                                                    <span className="sr-only">, {pivot.project.name}</span>
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    ))}
+
+                                    {content.length < 1 && (
+                                        <tr className="border-b border-gray-300 bg-white">
+                                            <td
+                                                colSpan={6}
+                                                className="py-4 pl-4 pr-3 text-sm font-bold text-gray-900 sm:pl-6 lg:pl-8">
+                                                No projects found.
+                                            </td>
+                                        </tr>
+                                    )}
+                                </tbody>
+                            </table>
+                            <NavigationPagination usePageInfo={pageInfo} contentSize={content.length} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
