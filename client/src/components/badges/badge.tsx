02b7b222915a4e11aa0ed88a9484b4603f85a670
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-11-21 06:37:59 -0300

    feat: add Badge component
    - Created a new `Badge` component to display badges with different variants.
    - Added `default` and `secondary` variants for badge styling.

---
+++ client/src/components/badges/badge.tsx
@@ -0,0 +1,16 @@
+import tw from "@/utils/tw"
+import { ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
+
+interface BadgeProps {
+    variant: "default" | "secondary"
+    children: ReactNode
+}
+
+export default function Badge({ variant, children }: Readonly<BadgeProps>) {
+    const badgeDefaultStyles = tw`inline-flex items-center rounded-full px-2 py-1 text-xs font-medium`
+    const badgeColors = variant === "default" ? tw`bg-gray-100 text-gray-600` : tw`bg-gray-800 text-gray-100`
+    const badgeStyles = twMerge(badgeDefaultStyles, badgeColors)
+
+    return <span className={badgeStyles}>{children}</span>
+}
