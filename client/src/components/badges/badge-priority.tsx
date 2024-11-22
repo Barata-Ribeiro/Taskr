5065b81d2bcc1e20386cae8c9f91b80d30cf5d45
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-11-22 06:51:19 -0300

    feat: add BadgePriority component and update Badge component
    - Created a new `BadgePriority` component to display task priority badges.
    - Updated `Badge` component to include `select-none` class for better user experience.

---
+++ client/src/components/badges/badge-priority.tsx
@@ -0,0 +1,30 @@
+import { TaskPriority } from "@/interfaces/task"
+import { getTaskPriorityColor } from "@/utils/get-color-functions"
+import tw from "@/utils/tw"
+import { twMerge } from "tailwind-merge"
+
+interface BadgePriorityProps {
+    priority: TaskPriority
+}
+
+export default function BadgePriority({ priority }: Readonly<BadgePriorityProps>) {
+    const priorityTitle = priority.toLowerCase()
+
+    const priorityTextColor = getTaskPriorityColor(priority)
+    const priorityBgColor = getTaskPriorityColor(priority).replace(/text/, "bg").replace(/\d+/, "100")
+    const fillPriorityColor = getTaskPriorityColor(priority).replace(/text/, "fill").replace(/\d+/, "500")
+
+    const wrapperStyles = tw`inline-flex select-none items-center gap-x-1.5 rounded-full px-2 py-1 font-heading text-xs font-medium capitalize`
+    const wrapperMergedStyles = twMerge(wrapperStyles, priorityBgColor, priorityTextColor)
+
+    const circleMergedStyles = twMerge("h-1.5 w-1.5", fillPriorityColor)
+
+    return (
+        <span className={wrapperMergedStyles}>
+            <svg viewBox="0 0 6 6" aria-hidden="true" className={circleMergedStyles}>
+                <circle r={3} cx={3} cy={3} />
+            </svg>
+            {priorityTitle}
+        </span>
+    )
+}
--- client/src/components/badges/badge.tsx
+++ client/src/components/badges/badge.tsx
@@ -8,7 +8,7 @@ interface BadgeProps {
 }
 
 export default function Badge({ variant, children }: Readonly<BadgeProps>) {
-    const badgeDefaultStyles = tw`inline-flex items-center rounded-full px-2 py-1 text-xs font-medium`
+    const badgeDefaultStyles = tw`inline-flex select-none items-center rounded-full px-2 py-1 text-xs font-medium`
     const badgeColors = variant === "default" ? tw`bg-gray-100 text-gray-600` : tw`bg-gray-800 text-gray-100`
     const badgeStyles = twMerge(badgeDefaultStyles, badgeColors)
 
