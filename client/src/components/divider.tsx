8c842de545cb956c08c546e3ade3f4c7139ff43d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-11-25 06:35:27 -0300

    feat: add Divider component
    - Created `Divider` component to visually separate content with a horizontal line and centered text.

---
+++ client/src/components/divider.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react"
+
+interface DividerProps {
+    children: ReactNode
+}
+
+export default function Divider({ children }: Readonly<DividerProps>) {
+    return (
+        <div className="relative">
+            <div aria-hidden="true" className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center">
+                <span className="bg-white px-2 text-gray-500">{children}</span>
+            </div>
+        </div>
+    )
+}
