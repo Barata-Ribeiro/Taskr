9243dc4fc3ed4ea52d08aba48c49d8405b86a251
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-11-27 06:16:48 -0300

    feat: add function to get task status style
    - Added `getTaskStatusStyle` function to return the appropriate style for task statuses.
    - Updated `getTaskStatusColor` function to return color names instead of text classes.

--- client/src/utils/get-color-functions.ts
+++ client/src/utils/get-color-functions.ts
@@ -16,18 +16,33 @@ export function getProjectStatusColor(status: ProjectStatus) {
     }
 }
 
+export function getTaskStatusStyle(status: TaskStatus) {
+    switch (status) {
+        case "OPEN":
+            return "text-green-700 bg-green-50 ring-green-600/20"
+        case "IN_PROGRESS":
+            return "text-yellow-800 bg-yellow-50 ring-yellow-600/20"
+        case "IN_REVIEW":
+            return "text-blue-600 bg-blue-50 ring-blue-500/10"
+        case "DONE":
+            return "text-gray-600 bg-gray-50 ring-gray-500/10"
+        default:
+            return "text-ebony-600 bg-ebony-50 ring-ebony-500/10"
+    }
+}
+
 export function getTaskStatusColor(status: TaskStatus) {
     switch (status) {
         case "OPEN":
-            return "text-green-500"
+            return "green"
         case "IN_PROGRESS":
-            return "text-yellow-500"
+            return "yellow"
         case "IN_REVIEW":
-            return "text-blue-500"
+            return "blue"
         case "DONE":
-            return "text-gray-500"
+            return "gray"
         default:
-            return "text-ebony-500"
+            return "ebony"
     }
 }
 
