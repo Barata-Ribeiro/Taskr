e3a64d6971aaa9ac6b58c902b1d8bf05dc770b7c
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-06-11 08:57:24 -0300

    feat: add user conversion method to UserBuilder for registration with password encoding

--- server/src/main/java/com/barataribeiro/taskr/user/UserBuilder.java
+++ server/src/main/java/com/barataribeiro/taskr/user/UserBuilder.java
@@ -1,17 +1,26 @@
 package com.barataribeiro.taskr.user;
 
+import com.barataribeiro.taskr.authentication.dto.RegistrationRequestDTO;
 import com.barataribeiro.taskr.user.dtos.UserSecurityDTO;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class UserBuilder {
     private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public UserSecurityDTO toUserSecurityDTO(User user) {
         return modelMapper.map(user, UserSecurityDTO.class);
+    }
+
+    public User toUser(RegistrationRequestDTO body) {
+        User user = modelMapper.map(body, User.class);
+        user.setPassword(passwordEncoder.encode(body.getPassword()));
+        return user;
     }
 }
