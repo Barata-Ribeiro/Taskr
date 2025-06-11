4320c9f946d7e23cec5dfc6e5f6e74c677a3f67f
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-06-11 08:11:41 -0300

    feat: add DTOs for Project and UserAuthor with validation annotations

---
+++ server/src/main/java/com/barataribeiro/taskr/project/dtos/ProjectDTO.java
@@ -0,0 +1,30 @@
+package com.barataribeiro.taskr.project.dtos;
+
+import com.barataribeiro.taskr.project.ProjectStatus;
+import com.barataribeiro.taskr.user.dtos.UserAuthorDTO;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+/**
+ * DTO for {@link com.barataribeiro.taskr.project.Project}
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProjectDTO implements Serializable {
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime dueDate;
+    private ProjectStatus status;
+    private UserAuthorDTO owner;
+    private Instant createdAt;
+    private Instant updatedAt;
+}
