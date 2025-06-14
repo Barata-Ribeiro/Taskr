6019fc465bb649fd23a97f57be11d393f5d416ff
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-06-14 08:33:58 -0300

    test: add integration tests for TaskController to validate task creation functionality

---
+++ server/src/test/java/com/barataribeiro/taskr/task/TaskControllerTest.java
@@ -0,0 +1,89 @@
+package com.barataribeiro.taskr.task;
+
+import com.barataribeiro.taskr.project.dtos.ProjectDTO;
+import com.barataribeiro.taskr.task.dtos.TaskDTO;
+import com.barataribeiro.taskr.task.dtos.TaskRequestDTO;
+import com.barataribeiro.taskr.user.UserBuilder;
+import com.barataribeiro.taskr.user.UserRepository;
+import com.barataribeiro.taskr.utils.TestSetupUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+class TaskControllerTest {
+    private static final TaskRequestDTO taskRequestDTO = new TaskRequestDTO();
+    private static String accessToken;
+    private static ProjectDTO defaultProject;
+    private static TaskDTO createdTask;
+    private final MockMvcTester mockMvcTester;
+
+    @BeforeAll
+    static void setUp(@Autowired @NotNull UserRepository userRepository,
+                      @Autowired @NotNull UserBuilder userBuilder,
+                      @Autowired @NotNull MockMvcTester mockMvcTester) throws Exception {
+        userRepository.deleteAll();
+
+        accessToken = TestSetupUtil.registerAndLoginDefaultUser(userRepository, userBuilder, mockMvcTester);
+        defaultProject = TestSetupUtil.createDefaultProject(mockMvcTester, accessToken).get();
+        assertNotNull(accessToken, "Access token should not be null");
+        assertNotNull(defaultProject, "Default project should not be null");
+    }
+
+    @Test()
+    @Order(1)
+    @DisplayName("It should create a task successfully")
+    void createTaskSuccessfully() throws Exception {
+        String description = "This is a test task description. It should be detailed enough to understand the task.";
+        final String dueDate = LocalDateTime.now().plusDays(30)
+                                            .format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
+
+        taskRequestDTO.setProjectId(defaultProject.getId());
+        taskRequestDTO.setTitle("Test Task");
+        taskRequestDTO.setDescription(description);
+        taskRequestDTO.setDueDate(dueDate);
+        taskRequestDTO.setStatus("TO_DO");
+        taskRequestDTO.setPriority("HIGH");
+
+        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().build();
+
+        mockMvcTester.post().uri("/api/v1/tasks")
+                     .header("Authorization", "Bearer " + accessToken)
+                     .contentType(MediaType.APPLICATION_JSON)
+                     .content(Jackson2ObjectMapperBuilder.json().build().writeValueAsBytes(taskRequestDTO))
+                     .assertThat()
+                     .hasStatus(HttpStatus.CREATED)
+                     .bodyJson()
+                     .satisfies(jsonContent -> {
+                         String json = jsonContent.getJson();
+
+                         assertEquals(taskRequestDTO.getTitle(), JsonPath.read(json, "$.data.title"));
+                         assertEquals(taskRequestDTO.getDescription(), JsonPath.read(json, "$.data.description"));
+                         assertEquals(taskRequestDTO.getDueDate(), JsonPath.read(json, "$.data.dueDate"));
+                         assertEquals(taskRequestDTO.getStatus(), JsonPath.read(json, "$.data.status"));
+                         assertEquals(taskRequestDTO.getPriority(), JsonPath.read(json, "$.data.priority"));
+
+                         createdTask = objectMapper.convertValue(JsonPath.read(json, "$.data"), TaskDTO.class);
+                     });
+    }
+}
--- server/src/test/java/com/barataribeiro/taskr/utils/TestSetupUtil.java
+++ server/src/test/java/com/barataribeiro/taskr/utils/TestSetupUtil.java
@@ -2,18 +2,26 @@ package com.barataribeiro.taskr.utils;
 
 import com.barataribeiro.taskr.authentication.dto.LoginRequestDTO;
 import com.barataribeiro.taskr.authentication.dto.RegistrationRequestDTO;
+import com.barataribeiro.taskr.project.dtos.ProjectDTO;
+import com.barataribeiro.taskr.project.dtos.ProjectRequestDTO;
 import com.barataribeiro.taskr.user.UserBuilder;
 import com.barataribeiro.taskr.user.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@Slf4j
 public class TestSetupUtil {
     public static String registerAndLoginDefaultUser(
             @NotNull UserRepository userRepository,
@@ -38,17 +46,45 @@ public class TestSetupUtil {
 
         userRepository.saveAll(List.of(userBuilder.toUser(body), userBuilder.toUser(secondBody)));
 
-        String[] accessToken = new String[1];
+        AtomicReference<String> accessToken = new AtomicReference<>();
         mockMvcTester.post().uri("/api/v1/auth/login")
                      .contentType(MediaType.APPLICATION_JSON)
                      .content(Jackson2ObjectMapperBuilder.json().build().writeValueAsBytes(loginBody))
                      .assertThat()
                      .bodyJson()
                      .satisfies(jsonContent -> {
-                         accessToken[0] = JsonPath.read(jsonContent.getJson(), "$.data.accessToken");
-                         assertNotNull(accessToken[0], "Access token should not be null");
+                         String json = jsonContent.getJson();
+                         accessToken.set(JsonPath.read(json, "$.data.accessToken"));
+                         assertNotNull(accessToken.get(), "Access token should not be null");
                      });
 
-        return accessToken[0];
+        return accessToken.get();
+    }
+
+    public static @NotNull AtomicReference<ProjectDTO> createDefaultProject(@NotNull MockMvcTester mockMvcTester,
+                                                                            @NotNull String accessToken) throws Exception {
+        ProjectRequestDTO projectRequestDTO = new ProjectRequestDTO();
+        projectRequestDTO.setTitle("Test Project");
+        projectRequestDTO.setDescription("This is a test project.");
+        projectRequestDTO.setDueDate(LocalDateTime.now().plusDays(30)
+                                                  .format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")));
+
+        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().build();
+
+        AtomicReference<ProjectDTO> createdProject = new AtomicReference<>();
+
+        mockMvcTester.post().uri("/api/v1/projects/create")
+                     .header("Authorization", "Bearer " + accessToken)
+                     .contentType(MediaType.APPLICATION_JSON)
+                     .content(Jackson2ObjectMapperBuilder.json().build().writeValueAsBytes(projectRequestDTO))
+                     .assertThat()
+                     .bodyJson()
+                     .satisfies(jsonContent -> {
+                         String json = jsonContent.getJson();
+                         createdProject.set(objectMapper.convertValue(JsonPath.read(json, "$.data"), ProjectDTO.class));
+                         assertNotNull(createdProject.get(), "Created project should not be null");
+                     });
+
+        return createdProject;
     }
 }
