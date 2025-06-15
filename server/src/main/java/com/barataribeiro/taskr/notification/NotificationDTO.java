7576f22ff97b29a4812a81573ca0829c51f2d55b
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-06-15 16:06:03 -0300

    feat: add NotificationDTO and NotificationService for websocket notifications

---
+++ server/src/main/java/com/barataribeiro/taskr/notification/NotificationDTO.java
@@ -0,0 +1,28 @@
+package com.barataribeiro.taskr.notification;
+
+import com.barataribeiro.taskr.user.dtos.UserAuthorDTO;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+/**
+ * DTO for {@link Notification}
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class NotificationDTO implements Serializable {
+    private Long id;
+    private String title;
+    private String message;
+    private NotificationType type;
+    private UserAuthorDTO recipient;
+    private boolean isRead;
+    private Instant createdAt;
+    private Instant updatedAt;
+}
---
+++ server/src/main/java/com/barataribeiro/taskr/notification/NotificationService.java
@@ -0,0 +1,16 @@
+package com.barataribeiro.taskr.notification;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class NotificationService {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public void sendNotificationThroughWebsocket(String username, NotificationDTO notification) {
+        messagingTemplate.convertAndSendToUser(username, "/notifications", notification);
+    }
+}
