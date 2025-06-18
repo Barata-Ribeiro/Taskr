5a9d38d84d8b384b9bff2283e90fa389599c4fef
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-06-18 07:55:13 -0300

    refactor: add additional exists methods in MembershipRepository for enhanced membership queries

--- server/src/main/java/com/barataribeiro/taskr/membership/MembershipRepository.java
+++ server/src/main/java/com/barataribeiro/taskr/membership/MembershipRepository.java
@@ -12,10 +12,15 @@ public interface MembershipRepository extends JpaRepository<Membership, Long>, J
     @EntityGraph(attributePaths = {"project", "user"})
     Optional<Membership> findByUser_UsernameAndProject_Id(String userUsername, Long projectId);
 
+    @EntityGraph(attributePaths = {"project", "user"})
     boolean existsByUser_UsernameAndProject_Id(String username, Long projectId);
 
+    @EntityGraph(attributePaths = {"project", "user"})
     boolean existsByUser_UsernameAndProject_IdAndRoleIs(String username, Long id, ProjectRole role);
-    
+
+    @EntityGraph(attributePaths = {"project.tasks", "user"})
+    boolean existsByUser_UsernameAndProject_Tasks_Id(String username, Long id);
+
     @EntityGraph(attributePaths = {"project", "user"})
     Streamable<Membership> findByProject_Id(Long id);
 }
