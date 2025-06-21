5987966264b006e46d02e4bb3887905d4c23c784
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-06-21 14:00:04 -0300

    chore: add Prettier and ESLint configuration files for code formatting and linting

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,16 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from "@eslint/eslintrc"
+import js from "@eslint/js"
+import { dirname } from "path"
+import { fileURLToPath } from "url"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript", "prettier")]
 
-export default eslintConfig;
+export default eslintConfig
--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    reactStrictMode: true,
+    trailingSlash: false,
+    skipTrailingSlashRedirect: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+                port: "",
+            },
+        ],
+    },
+    experimental: {
+        reactCompiler: true,
+        optimizePackageImports: ["tailwindcss", "@headlessui/react", "lucide-react"],
+    },
+}
 
-export default nextConfig;
+export default nextConfig
