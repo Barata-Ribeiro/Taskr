2deffdc90cd9d0e6ee50fb59dd6e576ae09284f3
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-06-28 15:00:46 -0300

    refactor: enhance Avatar component with size support and fallback handling

--- client/src/components/user/Avatar.tsx
+++ client/src/components/user/Avatar.tsx
@@ -1,19 +1,33 @@
 import getFallbackInitials from "@/utils/get-fallback-initials"
+import tw from "@/utils/tw"
 import Image from "next/image"
+import { twMerge } from "tailwind-merge"
 
 interface AvatarProps {
-    url?: string
+    url: string | null
     name: string
+    size?: "extra-small" | "small" | "medium" | "large" | "extra-large"
 }
 
-export default function Avatar({ url, name }: Readonly<AvatarProps>) {
-    if (url) {
-        return <Image src={url} alt={`Profile picture of ${name}`} className="rounded-full ring-2 ring-white" />
+export default function Avatar({ url, name, size = "small" }: Readonly<AvatarProps>) {
+    const sizeClasses = {
+        "extra-small": "size-6",
+        small: "size-8",
+        medium: "size-10",
+        large: "size-12",
+        "extra-large": "size-16",
     }
 
-    return (
-        <span className="inline-flex size-10 items-center justify-center rounded-full bg-gray-500">
-            <span className="font-medium text-white">{getFallbackInitials(name)}</span>
-        </span>
-    )
+    if (!url) {
+        const fallbackAvatarClasses = tw`inline-flex items-center justify-center rounded-full bg-gray-500`
+        return (
+            <span className={twMerge(fallbackAvatarClasses, sizeClasses[size])}>
+                <span className="font-medium text-white">{getFallbackInitials(name)}</span>
+            </span>
+        )
+    }
+
+    const avatarClasses = tw`rounded-full object-cover shadow-sm ring-2 ring-white`
+
+    return <Image src={url} alt={`Profile picture of ${name}`} className={twMerge(avatarClasses, sizeClasses[size])} />
 }
