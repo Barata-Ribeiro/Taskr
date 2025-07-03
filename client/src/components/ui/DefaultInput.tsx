f269535978082ecd5c716560cf6844c296cc8b13
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-07-03 08:52:03 -0300

    fix: adjust input styles for DefaultInput component to improve layout and dark mode support

--- client/src/components/ui/DefaultInput.tsx
+++ client/src/components/ui/DefaultInput.tsx
@@ -16,10 +16,10 @@ export default function DefaultInput({ label, description, ...props }: Readonly<
     const defaultStyles = {
         label: tw`block text-sm/6 font-medium data-disabled:opacity-50`,
         description: tw`text-sm/6 text-gray-500 data-disabled:opacity-50 dark:text-gray-400`,
-        input: tw`block w-full rounded-md border-none bg-white px-3 py-1.5 text-base ring-indigo-200 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:ring-2 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 data-disabled:bg-gray-100 sm:text-sm/6`,
+        input: tw`mt-2 block w-full rounded-md border-none bg-white px-3 py-1.5 text-base ring-indigo-200 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:ring-2 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 data-disabled:bg-gray-100 sm:text-sm/6`,
     }
 
-    const darkInputStyles = tw`dark:bg-white/5 dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:ring-indigo-500 dark:focus:outline-indigo-300`
+    const darkInputStyles = tw`dark:bg-white/5 dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:ring-indigo-500 dark:focus:outline-indigo-300 dark:disabled:bg-gray-700`
 
     const mergedInputStyles = twMerge(defaultStyles.input, darkInputStyles, props.className)
 
