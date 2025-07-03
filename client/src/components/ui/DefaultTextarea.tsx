a0832a5b06a71ca36da2ddb39a6129c68fb82073
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-07-03 08:51:25 -0300

    feat: add DefaultTextarea component with customizable label and description

---
+++ client/src/components/ui/DefaultTextarea.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import tw from "@/utils/tw"
+import { Description, Field, Label, Textarea } from "@headlessui/react"
+import { TextareaHTMLAttributes, useId } from "react"
+import { twMerge } from "tailwind-merge"
+
+interface DefaultTextareaProps extends Omit<TextareaHTMLAttributes<HTMLTextAreaElement>, "id"> {
+    label?: string
+    description?: string
+}
+
+export default function DefaultTextarea({ label, description, ...props }: Readonly<DefaultTextareaProps>) {
+    const defaultId = useId()
+
+    const defaultStyles = {
+        label: tw`block text-sm/6 font-medium data-disabled:opacity-50`,
+        description: tw`text-sm/6 text-gray-500 data-disabled:opacity-50 dark:text-gray-400`,
+        textarea: tw`mt-2 block w-full rounded-md border-none bg-white px-3 py-1.5 text-base ring-indigo-200 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:ring-2 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 data-disabled:bg-gray-100 sm:text-sm/6`,
+    }
+
+    const darkInputStyles = tw`dark:bg-white/5 dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:ring-indigo-500 dark:focus:outline-indigo-300 dark:disabled:bg-gray-700`
+
+    const mergedInputStyles = twMerge(defaultStyles.textarea, darkInputStyles, props.className)
+
+    return (
+        <Field disabled={props.disabled}>
+            {label && (
+                <Label htmlFor={defaultId} className={defaultStyles.label}>
+                    {label}
+                </Label>
+            )}
+            {description && <Description className={defaultStyles.description}>{description}</Description>}
+            <Textarea id={defaultId} {...props} className={mergedInputStyles} />
+        </Field>
+    )
+}
