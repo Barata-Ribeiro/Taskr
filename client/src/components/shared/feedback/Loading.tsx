8220e71915846e3039b708222f7f025822f9b78d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-07-04 05:59:31 -0300

    feat: add Loading and Spinner components for loading state indication

---
+++ client/src/components/shared/feedback/Loading.tsx
@@ -0,0 +1,10 @@
+import Spinner from "@/components/shared/Spinner"
+import { Fragment } from "react"
+
+export default function Loading() {
+    return (
+        <Fragment>
+            <Spinner /> Loading...
+        </Fragment>
+    )
+}
