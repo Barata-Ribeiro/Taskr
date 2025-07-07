029298895cbe8cf6493b92b531d7ce69d0a91cf8
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-07-07 06:50:02 -0300

    feat: implement createNewTask function for task creation with validation and error handling

---
+++ client/src/actions/task/create-new-task.ts
@@ -0,0 +1,50 @@
+"use server"
+
+import { ProblemDetails, RestResponse, State } from "@/@types/application"
+import { Task } from "@/@types/task"
+import ResponseError from "@/actions/application/response-error"
+import { invalidFormData, unauthenticated } from "@/actions/application/to-problem-details"
+import { createTaskUrl } from "@/helpers/backend-routes"
+import { taskCreateSchema } from "@/helpers/validation/task-schemas"
+import { auth } from "auth"
+import { revalidateTag } from "next/cache"
+
+export default async function createNewTask(state: State<unknown>, formData: unknown) {
+    const session = await auth()
+
+    try {
+        if (!session) return ResponseError(unauthenticated)
+
+        if (!(formData instanceof FormData)) return ResponseError(invalidFormData)
+
+        const rawFormData = Object.fromEntries(formData.entries())
+        const parsedFormData = taskCreateSchema.safeParse(rawFormData)
+
+        if (!parsedFormData.success) return ResponseError(parsedFormData.error)
+
+        const URL = createTaskUrl()
+
+        const response = await fetch(URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(parsedFormData.data),
+        })
+
+        const json = await response.json()
+
+        if (!response.ok) {
+            const problemDetails = json as ProblemDetails
+            return ResponseError(problemDetails)
+        }
+
+        const projectId = parsedFormData.data.projectId
+
+        revalidateTag(`tasks-project-${projectId}`)
+        revalidateTag(`project-${projectId}`)
+        revalidateTag(`my-projects-${session.user?.username}`)
+
+        return { ok: true, error: null, response: json as RestResponse<Task> }
+    } catch (e: unknown) {
+        return ResponseError(e)
+    }
+}
