1be5c51417757aa6aeb1d5a0ae3f2c7c79e45108
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-07-15 07:49:29 -0300

    refactor: add project and user statistics queries to repositories for enhanced data retrieval

--- server/src/main/java/com/barataribeiro/taskr/project/ProjectRepository.java
+++ server/src/main/java/com/barataribeiro/taskr/project/ProjectRepository.java
@@ -1,16 +1,46 @@
 package com.barataribeiro.taskr.project;
 
+import com.barataribeiro.taskr.stats.dtos.ProjectStatsDTO;
+import com.barataribeiro.taskr.stats.dtos.counts.ProjectsCountDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.util.UUID;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProjectRepository extends JpaRepository<Project, Long>, JpaSpecificationExecutor<Project> {
     @EntityGraph(attributePaths = {"owner"})
     Page<Project> findAllByOwner_Username(String ownerUsername, Pageable pageable);
 
-    long countByOwner_Id(UUID ownerId);
+    @Query("""
+           select new com.barataribeiro.taskr.stats.dtos.counts.ProjectsCountDTO(
+                count(distinct p),
+                sum(case when p.status = 'NOT_STARTED' then 1 else 0 end),
+                sum(case when p.status = 'IN_PROGRESS' then 1 else 0 end),
+                sum(case when p.status = 'COMPLETED' then 1 else 0 end),
+                sum(case when p.status = 'ON_HOLD' then 1 else 0 end),
+                sum(case when p.status = 'CANCELLED' then 1 else 0 end),
+                sum(case when p.dueDate < current_timestamp and p.status != 'COMPLETED' then 1 else 0 end))
+           from Project p
+           """)
+    ProjectsCountDTO getProjectsCount();
+
+    @Query("""
+           select new com.barataribeiro.taskr.stats.dtos.ProjectStatsDTO(
+                count(distinct t),
+                sum(case when t.status = 'TO_DO' then 1 else 0 end),
+                sum(case when t.status = 'IN_PROGRESS' then 1 else 0 end),
+                sum(case when t.status = 'DONE' then 1 else 0 end),
+                count(distinct c),
+                count(distinct m),
+                count(distinct a))
+           from Project p
+           left join p.tasks t
+           left join t.comments c
+           left join p.memberships m
+           left join p.feed a
+           where p.id = :projectId
+           """)
+    ProjectStatsDTO getProjectCount(Long projectId);
 }
--- server/src/main/java/com/barataribeiro/taskr/user/UserRepository.java
+++ server/src/main/java/com/barataribeiro/taskr/user/UserRepository.java
@@ -1,12 +1,12 @@
 package com.barataribeiro.taskr.user;
 
-import com.barataribeiro.taskr.user.enums.Roles;
+import com.barataribeiro.taskr.stats.dtos.UserStatsDTO;
+import com.barataribeiro.taskr.stats.dtos.counts.UserCountDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,11 +19,37 @@ public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificat
            """)
     Optional<User> findUserByUsernameOrEmailAllIgnoreCase(@Param("term") String term);
 
-    List<User> findAllByRole(Roles role);
-
     boolean existsByUsernameOrEmailAllIgnoreCase(String username, String email);
 
     long countByUsername(String username);
+
+    @Query("""
+           select new com.barataribeiro.taskr.stats.dtos.counts.UserCountDTO(
+               count(u),
+               sum(case when u.role = 'NONE' then 1 else 0 end),
+               sum(case when u.role = 'USER' then 1 else 0 end),
+               sum(case when u.role = 'ADMIN' then 1 else 0 end),
+               sum(case when u.role = 'BANNED' then 1 else 0 end),
+               sum(case when u.isVerified then 1 else 0 end),
+               sum(case when not u.isVerified then 1 else 0 end))
+           from User u
+           """)
+    UserCountDTO getUserCount();
+
+    @Query("""
+           select new com.barataribeiro.taskr.stats.dtos.UserStatsDTO(
+                count(p),
+                count(t),
+                count(c),
+                sum(case when m.role = 'MEMBER' then 1 else 0 end))
+           from User u
+           left join u.projects p
+           left join u.assignedTasks t
+           left join u.comments c
+           left join u.memberships m
+           where u.id = :userId
+           """)
+    UserStatsDTO getUserStats(UUID userId);
 
     long deleteByUsername(String username);
 }
