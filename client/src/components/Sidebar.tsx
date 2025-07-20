9d9041b342a5870a16ab5aae364e406bf3e01e57
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-07-20 11:09:44 -0300

    refactor: update Notifications icon in Sidebar component

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -2,7 +2,7 @@
 
 import Avatar from "@/components/user/Avatar"
 import { Button, Dialog, DialogBackdrop, DialogPanel, TransitionChild } from "@headlessui/react"
-import { ChartPieIcon, FolderIcon, HomeIcon, MailIcon, MenuIcon, XIcon } from "lucide-react"
+import { ChartPieIcon, FolderIcon, HomeIcon, InboxIcon, MenuIcon, XIcon } from "lucide-react"
 import { useSession } from "next-auth/react"
 import Image from "next/image"
 import Link from "next/link"
@@ -20,7 +20,7 @@ export default function Sidebar() {
 
     const navigation = [
         { name: "Dashboard", href: `${basePath}`, icon: HomeIcon },
-        { name: "Notifications", href: `${basePath}/notifications`, icon: MailIcon },
+        { name: "Notifications", href: `${basePath}/notifications`, icon: InboxIcon },
         { name: "Projects", href: `${basePath}/projects`, icon: FolderIcon },
         { name: "Reports", href: `${basePath}/reports`, icon: ChartPieIcon },
     ]
