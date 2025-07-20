6c2065fbe108965956858125e98e14168eb98e68
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-07-20 10:20:01 -0300

    feat: update DefaultCheckbox component to support optional label and ref

--- client/src/components/ui/DefaultCheckbox.tsx
+++ client/src/components/ui/DefaultCheckbox.tsx
@@ -2,18 +2,19 @@
 
 import tw from "@/utils/tw"
 import { Field, Input, Label } from "@headlessui/react"
-import { InputHTMLAttributes, useId } from "react"
+import { InputHTMLAttributes, Ref, useId } from "react"
 import { twMerge } from "tailwind-merge"
 
 interface DefaultCheckboxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "type" | "id" | "className"> {
-    label: string
+    label?: string
+    ref?: Ref<HTMLInputElement>
 }
 
-export default function DefaultCheckbox({ label, ...props }: Readonly<DefaultCheckboxProps>) {
+export default function DefaultCheckbox({ label, ref, ...props }: Readonly<DefaultCheckboxProps>) {
     const defaultId = useId()
 
     const defaultStyles = {
-        checkbox: tw`appearance-none rounded-sm border border-gray-300 bg-white ring-green-600 checked:border-green-600 checked:bg-green-600 indeterminate:border-green-600 indeterminate:bg-green-600 focus-visible:ring-green-600 focus-visible:outline-2 focus-visible:outline-offset-2 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto`,
+        checkbox: tw`cursor-pointer appearance-none rounded-sm border border-gray-300 bg-white ring-green-600 checked:border-green-600 checked:bg-green-600 indeterminate:border-green-600 indeterminate:bg-green-600 focus-visible:ring-green-600 focus-visible:outline-2 focus-visible:outline-offset-2 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto`,
         label: tw`block text-sm leading-6`,
     }
 
@@ -23,10 +24,12 @@ export default function DefaultCheckbox({ label, ...props }: Readonly<DefaultChe
 
     return (
         <Field className="inline-flex items-center gap-x-2">
-            <Input id={defaultId} type="checkbox" className={mergedCheckboxStyles} {...props} />
-            <Label htmlFor={defaultId} className={defaultStyles.label}>
-                {label}
-            </Label>
+            <Input id={defaultId} ref={ref} type="checkbox" className={mergedCheckboxStyles} {...props} />
+            {label && (
+                <Label htmlFor={defaultId} className={defaultStyles.label}>
+                    {label}
+                </Label>
+            )}
         </Field>
     )
 }
