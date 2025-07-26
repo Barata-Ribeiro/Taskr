087c32cc0b09837b5a0e2477f59dfe5a9577ff1a
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-07-26 09:43:32 -0300

    fix: add postcss package and configure serverExternalPackages in next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
     reactStrictMode: true,
     trailingSlash: false,
     skipTrailingSlashRedirect: true,
+    serverExternalPackages: ["postcss"],
     images: {
         remotePatterns: [
             {
