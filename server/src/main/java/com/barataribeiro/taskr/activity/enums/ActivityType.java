245dbbf533e86bc284a26dff127d41f089fbbd61
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-07-31 08:45:37 -0300

    refactor: update ActivityType enum to include completion status for task events

--- server/src/main/java/com/barataribeiro/taskr/activity/enums/ActivityType.java
+++ server/src/main/java/com/barataribeiro/taskr/activity/enums/ActivityType.java
@@ -1,19 +1,18 @@
 package com.barataribeiro.taskr.activity.enums;
 
 public enum ActivityType {
-    CREATE_PROJECT,
-    UPDATE_PROJECT,
-    DELETE_PROJECT,
-    ADD_TASK,
-    UPDATE_TASK,
-    DELETE_TASK,
-    ASSIGN_TASK,
-    UNASSIGN_TASK,
-    COMPLETE_TASK,
-    REOPEN_TASK,
-    ADD_COMMENT,
-    UPDATE_COMMENT,
-    DELETE_COMMENT,
-    ADD_MEMBER,
-    REMOVE_MEMBER
+    CREATE_PROJECT, // Done
+    UPDATE_PROJECT, // Done
+    ADD_TASK, // Done
+    UPDATE_TASK, // Done
+    DELETE_TASK, // Done
+    ASSIGN_TASK, // Todo: add assign event
+    UNASSIGN_TASK, // Todo: add unassign event
+    COMPLETE_TASK, // Todo: add complete event
+    REOPEN_TASK, // Todo: add reopen event
+    ADD_COMMENT, // Todo: add comment event
+    UPDATE_COMMENT, // Todo: add update comment event
+    DELETE_COMMENT, // Todo: add delete comment event
+    ADD_MEMBER, // Todo: add member event
+    REMOVE_MEMBER // Todo: add remove member event
 }
