783c5b645b5bff613f742b6df40dcef08de053f2
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-08-07 06:37:30 -0300

    feat: add method to update username for all associated activities

--- server/src/main/java/com/barataribeiro/taskr/activity/ActivityRepository.java
+++ server/src/main/java/com/barataribeiro/taskr/activity/ActivityRepository.java
@@ -1,12 +1,16 @@
 package com.barataribeiro.taskr.activity;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.*;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long>, JpaSpecificationExecutor<Activity> {
     @EntityGraph(attributePaths = {"project"})
     Page<Activity> findAllByProject_Id(Long projectId, Pageable pageable);
+
+    @Modifying
+    @Query("update Activity a set a.username = :newUsername where a.username = :oldUsername")
+    void updateUsernameForAllActivities(@Param("oldUsername") String oldUsername,
+                                        @Param("newUsername") String newUsername);
 }
