cc671bdd46429c8f9959f88986a585d172d27d4d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-08-16 09:05:27 -0300

    feat: enhance LatestUsers component with error handling and improved UI

--- client/src/components/admin/LatestUsers.tsx
+++ client/src/components/admin/LatestUsers.tsx
@@ -1,47 +1,106 @@
-import { Paginated } from "@/@types/application"
-import { User } from "@/@types/user"
+import { ProblemDetails } from "@/@types/application"
 import adminGetAllUsersPaginated from "@/actions/admin/admin-get-all-users-paginated"
+import DashboardErrorMessage from "@/components/shared/feedback/DashboardErrorMessage"
+import DefaultLinkButton from "@/components/ui/DefaultLinkButton"
+import Avatar from "@/components/user/Avatar"
+import Badge from "@/components/user/Badge"
 import dateFormatter from "@/utils/date-formatter"
+import { ChevronRightCircle } from "lucide-react"
+import { Session } from "next-auth"
+import Link from "next/link"
 
-export default async function LatestUsers() {
+export default async function LatestUsers({ session }: Readonly<{ session: Session | null }>) {
+    if (!session) return null
     const state = await adminGetAllUsersPaginated({ page: 0, perPage: 10, direction: "DESC", orderBy: "createdAt" })
 
-    const pagination = state.response?.data as Paginated<User>
+    if (!state.ok || !state.response?.data) {
+        const isProblemDetails = (state.error as ProblemDetails)?.type !== undefined
+        const errorMessage = isProblemDetails
+            ? (state.error as ProblemDetails).detail
+            : "An error occurred while fetching the users."
+        return <DashboardErrorMessage message={errorMessage} />
+    }
+
+    const baseUrl = `/dashboard/${session.user.username}`
+
+    const pagination = state.response?.data
     const content = pagination.content ?? []
 
     return (
-        <section>
-            <h2 className="mb-4 text-2xl font-bold">Latest Users</h2>
-            <div className="overflow-x-auto">
-                <table className="min-w-full border border-gray-200 bg-white">
-                    <thead>
-                        <tr>
-                            <th className="border-b border-gray-200 px-6 py-3 text-left text-xs leading-4 font-medium tracking-wider text-gray-500 uppercase">
-                                Username
-                            </th>
-                            <th className="border-b border-gray-200 px-6 py-3 text-left text-xs leading-4 font-medium tracking-wider text-gray-500 uppercase">
-                                Email
-                            </th>
-                            <th className="border-b border-gray-200 px-6 py-3 text-left text-xs leading-4 font-medium tracking-wider text-gray-500 uppercase">
-                                Created At
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {content.map(user => (
-                            <tr key={user.username}>
-                                <td className="whitespace-no-wrap border-b border-gray-200 px-6 py-4">
-                                    {user.username}
-                                </td>
-                                <td className="whitespace-no-wrap border-b border-gray-200 px-6 py-4">{user.email}</td>
-                                <td className="whitespace-no-wrap border-b border-gray-200 px-6 py-4">
-                                    {dateFormatter(user.createdAt)}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+        <section aria-labelledby="latest-users-section-title">
+            <header className="mb-4 border-b border-gray-200 pb-5 max-sm:space-y-2 sm:flex sm:items-center sm:justify-between dark:border-gray-700">
+                <h2 id="latest-users-section-title" className="text-2xl font-bold">
+                    Latest Users
+                </h2>
+                <DefaultLinkButton buttonType="color" width="fit" href={`${baseUrl}/admin/users`}>
+                    View All
+                </DefaultLinkButton>
+            </header>
+
+            <ul
+                className="divide-y divide-gray-100 overflow-hidden bg-white shadow-xs ring-1 ring-gray-200 sm:rounded-xl dark:bg-gray-800 dark:ring-gray-700"
+                aria-label="List of latest users">
+                {content.map(user => {
+                    const profileUrl = `${baseUrl}/profile/${user.username}`
+                    const profileLabel = `View profile of ${user.username}`
+                    const mailToUrl = `mailto:${user.email}`
+                    const mailToLabel = `Send email to ${user.email}`
+
+                    const isCurrentUser = session.user.username === user.username
+
+                    return (
+                        <li
+                            key={user.id}
+                            className="group relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6 lg:px-8 dark:hover:bg-gray-700">
+                            <div className="inline-flex min-w-0 items-center gap-x-4">
+                                <Avatar url={user.avatarUrl} name={user.username} size="small" />
+                                <div className="min-w-0 flex-auto">
+                                    <Link
+                                        href={profileUrl}
+                                        className="text-sm/6 font-semibold select-none"
+                                        aria-label={profileLabel}>
+                                        {user.username}{" "}
+                                        {isCurrentUser && <span className="text-xs text-indigo-500">(You)</span>}
+                                    </Link>
+
+                                    <Link
+                                        href={mailToUrl}
+                                        className="mt-1 flex text-xs/5 text-gray-500 hover:underline dark:text-gray-400"
+                                        aria-label={mailToLabel}>
+                                        {user.email}
+                                    </Link>
+                                </div>
+                            </div>
+
+                            <div className="inline-flex shrink-0 items-center gap-x-4">
+                                <div className="hidden sm:flex sm:flex-col sm:items-end">
+                                    <Badge userRole={user.role} />
+                                    <time
+                                        className="mt-1 text-xs/5 text-gray-500 dark:text-gray-400"
+                                        dateTime={user.createdAt.toString()}
+                                        aria-label={`Joined on ${dateFormatter(user.createdAt)}`}>
+                                        {dateFormatter(user.createdAt)}
+                                    </time>
+                                </div>
+
+                                <DefaultLinkButton
+                                    href={profileUrl}
+                                    buttonType="ghost"
+                                    width="fit"
+                                    isIconOnly
+                                    aria-label={profileLabel}
+                                    title={profileLabel}>
+                                    <ChevronRightCircle
+                                        aria-hidden="true"
+                                        size={20}
+                                        className="flex-none text-gray-400"
+                                    />
+                                </DefaultLinkButton>
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
         </section>
     )
 }
