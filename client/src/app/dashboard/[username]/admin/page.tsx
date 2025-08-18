90cc1e9d83f5e88b00b88757bffa390cefc86440
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-08-18 10:17:45 -0300

    feat: add header to Admin Panel with welcome message and description

--- client/src/app/dashboard/[username]/admin/page.tsx
+++ client/src/app/dashboard/[username]/admin/page.tsx
@@ -26,6 +26,18 @@ export default async function AdminPage({ params }: Readonly<AdminPageProps>) {
 
     return (
         <Fragment>
+            <header className="mx-auto max-w-2xl lg:text-center">
+                <h2 className="text-base/7 font-semibold text-indigo-600 dark:text-indigo-300">
+                    Welcome, {session.user.username}!
+                </h2>
+                <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty sm:text-5xl lg:text-balance">
+                    Admin Panel
+                </p>
+                <p className="mt-6 text-lg/8 text-gray-700 dark:text-gray-400">
+                    Here you can manage users, projects, and other administrative tasks.
+                </p>
+            </header>
+
             <Suspense fallback={<LatestUsersSkeleton />}>
                 <LatestUsers session={session} />
             </Suspense>
